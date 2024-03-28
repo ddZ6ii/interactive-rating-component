77226aa4a0f75b6aa57e22922e6fedbdff07c6ad
Denis Dezest <denis.dezest@gmail.com>
2024-03-28 01:37:59 +0100

    ♿️ Improve accessibility

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,41 +1,98 @@
-export const focusRadioById = (radioId) =>
-  document.querySelector(`${radioId}`).focus();
+const RADIO_STATUS = {
+  isValid: false,
+  errorMessage: 'Please select a rating to continue.',
+};
 
-export const validateRadioByName = (radioName) => {
-  const selection =
-    document.querySelector(`input[name=${radioName}]:checked`)?.value ?? '';
-  const isValid = selection.length !== 0;
+export const FORM_STATUS = {
+  submit: 'submit',
+  error: 'error',
+};
+
+// Update user feedback (error message and visibility).
+export const updateFeedback = (controlEl, status) => {
+  const errorEl = controlEl.nextElementSibling;
+  errorEl.innerText = status.errorMessage;
+  errorEl.classList.toggle('hidden', status.isValid);
+};
+
+// Single radio input validation.
+export const validateRadio = (radioEl) => {
+  if (!(radioEl instanceof Element) || radioEl.type !== 'radio')
+    throw new Error('Invalid radio input!');
+
+  const isValid = radioEl.checked;
   return {
+    ...RADIO_STATUS,
     isValid,
-    errorMessage: isValid ? '' : 'Please select a rating to continue.',
+    errorMessage: isValid ? '' : RADIO_STATUS.errorMessage,
   };
 };
 
-export const updateFeedback = (nodeId, status) => {
-  const errorEl = document.querySelector(`${nodeId}`);
+// Multiple radio inputs validation.
+export const validateRadioGroup = (radioGroupEl) => {
+  if (
+    (!radioGroupEl) instanceof Element ||
+    [...radioGroupEl.querySelectorAll("input[type='radio']")].length === 0 ||
+    [...radioGroupEl.querySelectorAll("input[type='radio']")].some(
+      (radioEl) => (!radioEl) instanceof Element,
+    )
+  )
+    throw new Error('Invalid radio group!');
+
+  const radioEls = [...radioGroupEl.querySelectorAll("input[type='radio']")];
+  const radioStatus = radioEls.reduce((acc, radioEl) => {
+    const currentStatus = validateRadio(radioEl);
+    return currentStatus.isValid ? currentStatus : acc;
+  }, RADIO_STATUS);
+
   // Update error message.
-  errorEl.querySelector(`${nodeId}-message`).innerText = status.errorMessage;
-  // Update error visibility.
-  errorEl.classList.toggle('hidden', status.isValid);
-  // Update submit button state.
-  document
-    .querySelector('button[type="submit"]')
-    .toggleAttribute('disabled', !status.isValid);
+  updateFeedback(radioGroupEl, radioStatus);
+
+  // Update ARIA attribute for accessibility.
+  radioEls.forEach((radioEl) =>
+    radioEl.setAttribute('aria-invalid', `${!radioStatus.isValid}`),
+  );
+
+  return radioStatus;
 };
 
+// Update form controls state and visibility.
+export const updateFormStatus = (formEl, formStatus) => {
+  if ((!formEl) instanceof Element)
+    throw new Error('Invalid form node element!');
+
+  if (!Object.values(FORM_STATUS).includes(formStatus))
+    throw new Error('Invalid form status!');
+
+  const isSubmitting = formStatus === FORM_STATUS.submit;
+  const hasError = formStatus === FORM_STATUS.error;
+  const radioGroup = formEl.querySelector('.radio-group');
+  const radios = [...radioGroup.querySelectorAll("input[type='radio']")];
+  const btn = formEl.querySelector('button[type="submit"]');
+  const btnLoader = btn.querySelector('.loader');
+  const btnLabel = btn.querySelector('.label');
+
+  radioGroup.disabled = isSubmitting;
+  btn.disabled = isSubmitting;
+  btnLoader.classList.toggle('hidden', !isSubmitting);
+  btnLabel.classList.toggle('hidden', isSubmitting);
+  // Invalid form data -> focus 5 stars radio input.
+  if (hasError) radios.at(-1).focus();
+};
+
+// Dymanicall swap content on successfull form submission.
 export const swapPages = (pages) => {
   let currentIndex = 0;
 
-  // Check for user's motion preferences.
   const hasMotionDisabled = window.matchMedia(
     '(prefers-reduced-motion: reduce)',
   ).matches;
 
-  // Swap pages without animation.
+  // Swap pages without animation if motion disabled in browser.
   if (hasMotionDisabled) {
-    pages[currentIndex].classList.add('hidden');
-    currentIndex += 1;
-    pages[currentIndex].classList.remove('hidden');
+    pages.forEach((page, index) =>
+      page.classList.toggle('hidden', index === currentIndex),
+    );
     return;
   }
 
@@ -50,5 +107,24 @@ export const swapPages = (pages) => {
     // 2.3. ... finally animate-in the new page.
     currentIndex += 1;
     pages[currentIndex].classList.remove('hidden');
+  });
+};
+
+// Ensure all pages have the same height.
+export const normalizePageHeight = (pages) => {
+  // Get max. pages height.
+  const maxHeight = pages.reduce((max, page) => {
+    // Display all pages momentarily to compute initial heights.
+    page.classList.remove('hidden');
+    return Math.max(page.offsetHeight, max);
+  }, 0);
+
+  // Adjust page height.
+  pages.forEach((page, index) => {
+    // First copy reference prior to modifying the property to satisfy the linter.
+    const pageCopy = page;
+    pageCopy.style.minHeight = `${maxHeight}px`;
+    // Hide back all pages except the current one.
+    if (index !== 0) page.classList.add('hidden');
   });
 };
--- src/main.js
+++ src/main.js
@@ -1,88 +1,63 @@
 import * as utils from './js/utils';
 
-const RADIO_NAME = 'rating';
-const form = document.querySelector('#rating-form');
-const radios = [...form.querySelectorAll("input[type='radio']")];
 const pages = [...document.querySelectorAll('.page-content')];
+const form = document.querySelector('#rating-form');
+const radioGroup = form.querySelector('[role="radiogroup"');
+const radios = [...radioGroup.querySelectorAll("input[type='radio']")];
+const btn = form.querySelector('button[type="submit"]');
 
-// Check if validation is required.
 const isValidationRequired = radios.some((radio) =>
   radio.hasAttribute('required'),
 );
 
-// Ensure all pages have the same height.
-window.addEventListener('load', () => {
-  // Get max. pages height.
-  const maxHeight = pages.reduce((max, page) => {
-    // Display all pages momentarily.
-    page.classList.remove('hidden');
-    return Math.max(page.offsetHeight, max);
-  }, 0);
-
-  // Adjust page height.
-  pages.forEach((page, index) => {
-    // Fisrt copy by reference prior to modifying the property to satisfy the linter.
-    const pageCopy = page;
-    pageCopy.style.minHeight = `${maxHeight}px`;
-    // Hide back all pages except the current one.
-    if (index !== 0) page.classList.add('hidden');
+const validateForm = () =>
+  new Promise((resolve, reject) => {
+    if (isValidationRequired) {
+      const { isValid } = utils.validateRadioGroup(radioGroup);
+      if (!isValid) reject(Error('invalid form data'));
+    }
+    // Pretend its hitting the network.
+    setTimeout(() => {
+      resolve();
+    }, 1000);
   });
+
+const submitForm = (formData) => {
+  const rating = formData.get('rating') ?? 'x';
+  utils.swapPages(pages);
+  pages[1].querySelector(`#selected-rating`).innerText = rating;
+};
+
+// Disable browser's built-in validation (done programatically to ensure browser validation can still occur if JavaScript is disabeld in the browser).
+form.noValidate = true;
+
+window.addEventListener('load', () => {
+  // 1. Enable form controls (JavaScript enabled).
+  radioGroup.disabled = false;
+  btn.disabled = false;
+  // 2. Ensure all pages have the same height.
+  utils.normalizePageHeight(pages);
 });
 
-// Discard browser built-in validation.
-form.setAttribute('novalidate', '');
-
-// Handle submission process with custom form data validation (verify input on submit).
-form.addEventListener('submit', async (evt) => {
-  const validateForm = (e) =>
-    new Promise((resolve, reject) => {
-      if (isValidationRequired) {
-        // 1. Prevent form submission.
-        e.preventDefault();
-
-        // 2. Validate radio input.
-        const radioStatus = utils.validateRadioByName(`${RADIO_NAME}`);
-
-        // 3. Update UI.
-        utils.updateFeedback(`#${RADIO_NAME}-error`, radioStatus);
-
-        // 4. Reject promise if invalid data.
-        if (!radioStatus.isValid) {
-          reject(Error('invalid form data'));
-          return;
-        }
-      }
-
-      // Return form data.
-      const rating = new FormData(form).get(`${RADIO_NAME}`) ?? 'x';
-      resolve(rating);
-    });
-
-  const submitForm = (rating) => {
-    // 1. Reset form.
-    form.reset();
-
-    // 2. Switch page display on screen.
-    utils.swapPages(pages);
-
-    // 3. Update UI with selected rating.
-    pages[1].querySelector(`#selected-${RADIO_NAME}`).innerText = rating;
-  };
-
+// Form submission with on submit custom data validation.
+form.addEventListener('submit', async (e) => {
   try {
-    const rating = await validateForm(evt);
-    submitForm(rating);
+    e.preventDefault();
+    // Retrieve form data prior to disabling form controls (otherwise form data will not be submitted!).
+    const formData = new FormData(form);
+    utils.updateFormStatus(e.target, utils.FORM_STATUS.submit);
+    await validateForm();
+    submitForm(formData);
   } catch (err) {
-    // Invalid form data -> focus radio input
-    utils.focusRadioById(`#${RADIO_NAME}-5`);
+    utils.updateFormStatus(form, utils.FORM_STATUS.error);
   }
 });
 
-// Update form errors and submit button state (live).
+// Remove displayed error upon radio selection.
 form.addEventListener('change', (e) => {
-  if (!isValidationRequired || e.target.name !== `${RADIO_NAME}`) return;
-  const radioStatus = utils.validateRadioByName(e.target.name);
+  if (!isValidationRequired || e.target.name !== 'rating') return;
+  const radioStatus = utils.validateRadio(e.target);
   if (radioStatus.isValid) {
-    utils.updateFeedback(`#${RADIO_NAME}-error`, radioStatus);
+    utils.updateFeedback(radioGroup, radioStatus);
   }
 });
